ccd103190df08eeff5d8936bf662b5678fcb1f48
Vladimir <156834603+Vvvoan@users.noreply.github.com>
2024-03-27 20:35:15 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,29 @@
+        }
+        for (int i = 0; i < 10; i++) {
+    public static void main(String[] args) {
+
+        }
+            data[i] = (int) (Math.random() * 100) + 1;
+        int[] data = new int[10];
+public class RandomDataGenerator {
+    }
+}
+            System.out.println("Random Number: " + item);
+        for (int item : data) {
+    for (let i = 0; i < 10; i++) {
+}
+
+function generateRandomData() {
+
+    data.forEach(item => {
+main();
+        data.push(Math.floor(Math.random() * 100) + 1);
+    const data = generateRandomData();
+
+    const data = [];
+        console.log("Random Number: " + item);
+    return data;
+}
+    }
+    });
+function main() {
